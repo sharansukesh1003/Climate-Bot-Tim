285caffb6a59d0b7d25ca4730b4eb32d81f0f328
sharansukesh1003 <sharansukesh2000@gmail.com>
2022-01-03 11:47:44 +0530

    procfile added

--- bot.js
+++ bot.js
@@ -1,16 +1,14 @@
 require('dotenv').config()
 const { climateNews } = require("./services/post")
-const express = require('express')
 var CronJob = require('cron').CronJob
 const Instagram = require('instagram-web-api')
-const { post } = require('./services/insta')
+const { post } = require('./services/instagram')
 var FileCookieStore = require("tough-cookie-filestore2")
 const cookieStore = new FileCookieStore('./cookies.json')
 
 const password = process.env.IG_PASSWORD
 const username = process.env.IG_USERNAME
 
-const app = express()
 const client = new Instagram({ username, password, cookieStore }, { language: 'en:us' })
 
 var job = new CronJob('0 0 */6 * * *', async () => {
