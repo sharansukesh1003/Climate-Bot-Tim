6e70e0be1270ea1bd904bbd5107c7a4971e0e868
Sharan Sukesh <77352136+sharansukesh1003@users.noreply.github.com>
2022-03-10 16:56:25 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const username = process.env.IG_USERNAME
 
 const client = new Instagram({ username, password, cookieStore }, { language: 'en:us' })
 
-var job = new CronJob('0 */1 * * * *', async () => {
+var job = new CronJob('0 0 */24 * * *', async () => {
     console.log("started")
     const postData = await climateNews()
     post(client, postData)
